18ad4ddddf5c95b0878edc8d9979a15efe7837ac
Dennis Neumann <neumann@sub.uni-goettingen.de>
2018-06-16 13:44:37 +0200

    Add some more view elements

--- app/src/main/java/dn/einmaleins/MainActivity.java
+++ app/src/main/java/dn/einmaleins/MainActivity.java
@@ -3,6 +3,7 @@ package dn.einmaleins;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void bla(View view) {
+    public void generateExercise(View view) {
         calc.createNewExercise();
         String exerciseString = calc.getMultiExercise();
         TextView tv = findViewById(R.id.textView);
         tv.setText(exerciseString);
+
+        Button buttonToHide = findViewById(R.id.button);
+        Button buttonToShow = findViewById(R.id.button2);
+
+        buttonToHide.setVisibility(View.INVISIBLE);
+        buttonToShow.setVisibility(View.VISIBLE);
+    }
+
+    public void showAnswer(View view) {
+        String answerString = calc.getMultiAnswer() + " " + calc.getHint();
+        TextView tv = findViewById(R.id.textView2);
+        tv.setText(answerString);
+
+        Button buttonToHide = findViewById(R.id.button2);
+        Button buttonToShow = findViewById(R.id.button);
+
+        buttonToHide.setVisibility(View.INVISIBLE);
+        buttonToShow.setVisibility(View.VISIBLE);
+
     }
 }
