5bb2a422683620ce7371c3dabd9080520331793b
Dennis Neumann <neumann@sub.uni-goettingen.de>
2018-06-18 10:33:30 +0200

    Refactor

--- app/src/main/java/dn/einmaleins/MainActivity.java
+++ app/src/main/java/dn/einmaleins/MainActivity.java
@@ -3,8 +3,6 @@ package dn.einmaleins;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -20,18 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void generateExercise(View view) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    public void generateExercise(View view) {
         List<State> newStates = stateChanger.generateExercise();
         applyNewStates(newStates);
     }
 
-    public void showAnswer(View view) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    public void showAnswer(View view) {
         List<State> newStates = stateChanger.showAnswer();
         applyNewStates(newStates);
     }
 
 
-    private void applyNewStates(List<State> newStates) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    private void applyNewStates(List<State> newStates) {
         for (State state : newStates) {
             applyState(state);
         }
@@ -40,10 +38,10 @@ public class MainActivity extends AppCompatActivity {
     private <T extends View> void applyState(State state) {
         try {
             T genericView = findViewById(R.id.class.getField(state.id).getInt(null));
-            Map<String, Object> viewProperties = state.props;
-            for (Map.Entry<String, Object> entry : viewProperties.entrySet()) {
-                String methodName = entry.getKey();
-                Object value = entry.getValue();
+            List<State.Property> viewProperties = state.props;
+            for (State.Property prop : viewProperties) {
+                String methodName = prop.key;
+                Object value = prop.value;
                 Class<T> genericViewClass = state.clazz;
                 Class<?> valueClass;
                 if (value instanceof String) {
--- app/src/main/java/dn/einmaleins/State.java
+++ app/src/main/java/dn/einmaleins/State.java
@@ -1,13 +1,15 @@
 package dn.einmaleins;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class State<T> {
 
     public String id;
     public Class<T> clazz;
-    public Map<String, Object> props = new HashMap<>();
+    public List<Property> props = new ArrayList<>();
 
     public static <T> State create(String id, Class<T> clazz) {
         State s = new State();
@@ -17,7 +19,18 @@ public class State<T> {
     }
 
     public State with(String methodName, Object value) {
-        props.put(methodName, value);
+        props.add(new Property(methodName, value));
         return this;
+    }
+
+    public static class Property {
+
+        public String key;
+        public Object value;
+
+        public Property(String key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
